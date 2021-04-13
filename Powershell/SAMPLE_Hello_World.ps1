437839718dd017233043295837091787c07fd008
EGBERTO <egberto.zanon@surrey.ca>
2021-04-13 09:28:05 -0700

    Create SAMPLE_Hello_World.ps1

---
+++ Powershell/SAMPLE_Hello_World.ps1
@@ -0,0 +1,4 @@
+# Sample Hello World Script
+# Author: Egberto Zanon
+# This script Prints Hello World Colorized
+ Write-host -nonewline "HELLO" ;Write-host -f red -nonewline " W";Write-host -f yellow -nonewline "o";Write-host -f green -nonewline "r"; Write-host -f blue -nonewline "d"; Write-host -nonewline "!!!!"
